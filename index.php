956fccef55e8e417062eea591ed69d0fcd465c19
emanuel <emanuelmerlo15@gmail.com>
2023-06-10 09:11:54 -0600

    tercer commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'modelo/provincia.php';
+require_once 'modelo/direccion.php';
+require_once 'modelo/titular.php';
+require_once 'modelo/lineaprestamos.php';
+require_once 'modelo/prestamos.php';
--- modelo/prestamo.php
+++ modelo/prestamo.php
@@ -10,9 +10,25 @@ class prestamo{
 
     public function mostrardatos()
     {
-        echo 'id:'. $this-> . '<br>';
-        echo 'fecha de alta :'. $this-> . '<br>';
-        echo 'cantidadcuotas'. $this-> . '<br>';
-        echo 'montocapital'. $this-> . '<br>';
+        echo 'id:'. $this->id . '<br>';
+        echo 'fecha de alta :'. $this->fechaalta . '<br>';
+        echo 'cantidadcuotas'. $this->cantidadcuotas . '<br>';
+        echo 'montocapital'. $this->montocapital . '<br>';
+
+        echo 'id:'. $this->titular->id . '<br>';
+        echo 'nombre:'. $this->titular->nombre . '<br>';
+        echo 'apellido:'. $this->titular->apellido . '<br>';
+        echo 'documento'. $this->titular->documento . '<br>';
+        echo 'id:'. $this->titular->direccion->id . '<br>';
+        echo 'calle:'. $this->titular->direccion->calle . '<br>';
+        echo 'numero:'. $this->titular->direccion->numero . '<br>';
+        echo 'localidad:'. $this->titular->direccion->localidad . '<br>';
+        echo 'id:'. $this->titular->direccion->provincia->id . '<br>';
+        echo 'descripcion:'. $this->titular->direccion->provincia->descripcion . '<br>';
+
+
+        echo 'id:'. $this->lineaprestamo->id . '<br>';
+        echo 'descripcion'. $this->lineaprestamo->descripcion . '<br>';
+        echo 'taza efectiva mensual'. $this->tazaefectivamensual . '<br>';
     }
 }
